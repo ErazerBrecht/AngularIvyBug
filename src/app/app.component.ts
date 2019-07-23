36f91a295d02218213942dac8716956342bdfdfa
Brecht Carlier <erazerbrecht@gmail.com>
2019-07-23 20:59:28 +0200

    Added repro of #31785

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ComponentFactoryResolver, ViewChild, ViewContainerRef, Type } from '@angular/core';
+import { HelloComponent } from './components/hello/hello.component';
+import { WorldComponent } from './components/world/world.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularIvyRegression';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container', { read: ViewContainerRef, static: false }) container: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  public ngAfterViewInit() {
+    this.createComponent(HelloComponent);
+    this.createComponent(WorldComponent);
+  }
+
+  private createComponent<T>(component: Type<T>) {
+    const factory = this.resolver.resolveComponentFactory(component);
+    this.container.createComponent(factory);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './components/hello/hello.component';
+import { WorldComponent } from './components/world/world.component';
 
 @NgModule({
+  entryComponents: [
+    HelloComponent,
+    WorldComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule
